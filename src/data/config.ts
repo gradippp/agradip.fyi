c815414aa82c68d10265195b51b05ac3de285856
gradippp <me@agradip.fyi>
2025-11-23 23:27:47 +0530

    Added not found page

--- src/data/config.ts
+++ src/data/config.ts
@@ -2,4 +2,5 @@ export const CONFIG = {
   TWEMOJI_CDN: "https://files.agradip.fyi/cdn/twemoji-assets/",
   PALE_BLUE_DOT_MP4: "https://files.agradip.fyi/u/b4ce0282.mp4",
   PALE_BLUE_DOT_PNG: "https://files.agradip.fyi/u/d28b41c3.png",
+  AGRADIP_BUNNY_GIF: "https://files.agradip.fyi/u/44ba13d0.gif",
 };
